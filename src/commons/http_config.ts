1b8a65ba4a6dd3e79cc894a0ae98daf186888f54
Adish Agarwal <adishag@google.com>
2024-01-24 15:16:37 +0000

    flipping endpoint to prod

--- src/commons/http_config.ts
+++ src/commons/http_config.ts
@@ -15,9 +15,9 @@
  */
 
 export const VALIDATE_ENDPOINT_DOMAIN =
-  'https://staging-securityposture-googleapis.sandbox.google.com/v1alpha';
+  'https://securityposture.googleapis.com/v1alpha';
 export const VALIDATE_ENDPOINT_PATH = (orgId: string) =>
   `/organizations/${orgId}/locations/global/reports:createIaCValidationReport`;
 export const RETRIABLE_ERROR_CODES = [408, 429, 500, 502, 503, 504];
 export const MAX_RETRIES_FOR_INITIATE_SCAN = 3;
-export const MAX_RETRIES_FOR_POLLING = 10;
+export const MAX_RETRIES_FOR_POLLING = 50;
